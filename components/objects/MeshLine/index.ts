9dc2a96440a8b675193e151f085abea980a0882e
Ajasra <betinvasilij@gmail.com>
2024-11-29 19:31:27 +0800

    - fix error

--- components/objects/MeshLine/index.ts
+++ components/objects/MeshLine/index.ts
@@ -1,5 +1,4 @@
 import { extend } from "@react-three/fiber";
 import { MeshLine, MeshLineMaterial } from "three.meshline";
-import { Line2, LineMaterial, LineGeometry } from "three/examples/jsm/lines/Line2";
 
-extend({ Line2, LineMaterial, LineGeometry, MeshLine, MeshLineMaterial });
+extend({ MeshLineMaterial });
